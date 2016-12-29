db0440f1cf6c9aba21e4d56fe6b2039494258a63
Mehri Russo <mehrimova@gmail.com>
2016-12-29 09:39:39 -0700

    update app.js file

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 'use strict';
+
+var me = {
+  firstname:'Mehri',
+  lastname: 'Russo',
+  address: {
+    street: 'APT C',
+    city: 'Boulder',
+    state: 'CO'
+  }
+};
+
+function greet(person) {
+  console.log('Hi ' + person.firstname);
+}
+
+greet(me);
+
+greet({
+  firstname: 'Jack',
+  lastname: 'Russo'
+});
